b9441ea7d926fcd4e4627c96bb924782c5338ced
andrew.olson <ajolson7@gmail.com>
2017-09-28 10:43:28 -0600

    Your mom's GUI has been added.  Meaning the 'Join Game' GUI has been added.

---
+++ client/TicketToRideApp/app/src/main/java/smarthawkdesigns/tickettorideapp/JoinGame.java
@@ -0,0 +1,13 @@
+package com.example.ajols.ticket_to_ride_gui_setup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_game);
+    }
+}

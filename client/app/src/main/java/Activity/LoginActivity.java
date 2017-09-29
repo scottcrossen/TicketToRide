d7c032aa7fbc0214ee5e1d03c249d96acad44df4
spj456 <spj432@gmail.com>
2017-09-29 13:29:24 -0600

    Added Activity Files

--- client/app/src/main/java/Activity/LoginActivity.java
+++ client/app/src/main/java/Activity/LoginActivity.java
@@ -3,7 +3,9 @@ package Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import Model.*;
 import Fragment.LoginFragment;
 
 /**
@@ -16,27 +18,15 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         FragmentManager fm = this.getSupportFragmentManager();
-        loginFragment = (LoginFragment) fm.findFragmentById(R.id.loginFragment);
-        ModelApp.SINGLETON.setIconMapPart("n");
-        if (ModelApp.SINGLETON.getCurrentUser() == null) {
-            ModelApp.SINGLETON.start(this);
-            if (loginFragment == null) {
-                loginFragment = LoginFragment.newInstance();
-                fm.beginTransaction()
-                        .add(R.id.loginFragment, loginFragment)
-                        .commit();
-            }
-        } else {
-            FragmentManager fm2 = this.getSupportFragmentManager();
-            mapFragment = (MapFragment) fm2.findFragmentById(R.id.mapFragment);
-            if (mapFragment == null) {
-                mapFragment = MapFragment.newInstance();
-                fm.beginTransaction()
-                        .add(R.id.mapFragment, mapFragment)
-                        .commit();
-            }
+
+        LoginFragment loginFragment = (LoginFragment) fm.findFragmentById(R.id.loginFragment);
+
+        if (loginFragment == null) {
+            fm.beginTransaction()
+                    .add(R.id.personFragment, new LoginFragment())//.newInstance(p))
+                    .commit();
         }
     }
 }

3d41465142fbf0fce88d82eb7832b006dfaedaa4
spj456 <spj432@gmail.com>
2017-09-29 13:00:17 -0600

    Started Login

--- client/app/src/main/java/loginActivity.java
+++ client/app/src/main/java/loginActivity.java
@@ -1,5 +1,40 @@
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Seth on 9/29/2017.
  */
-public class loginActivity {
+public class loginActivity extends AppCompatActivity {
+
+    private LoginFragment loginFragment;
+
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fm = this.getSupportFragmentManager();
+        loginFragment = (LoginFragment) fm.findFragmentById(R.id.loginFragment);
+        ModelApp.SINGLETON.setIconMapPart("n");
+        if (ModelApp.SINGLETON.getCurrentUser() == null) {
+            ModelApp.SINGLETON.start(this);
+            if (loginFragment == null) {
+                loginFragment = LoginFragment.newInstance();
+                fm.beginTransaction()
+                        .add(R.id.loginFragment, loginFragment)
+                        .commit();
+            }
+        } else {
+            FragmentManager fm2 = this.getSupportFragmentManager();
+            mapFragment = (MapFragment) fm2.findFragmentById(R.id.mapFragment);
+            if (mapFragment == null) {
+                mapFragment = MapFragment.newInstance();
+                fm.beginTransaction()
+                        .add(R.id.mapFragment, mapFragment)
+                        .commit();
+            }
+        }
+    }
 }

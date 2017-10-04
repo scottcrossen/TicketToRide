d0954e9d37a0137524542ecaedd534c62bf0f095
spj456 <spj432@gmail.com>
2017-10-04 13:29:26 -0600

    login

--- app/src/main/java/teamseth/cs340/tickettoride/Activity/LoginActivity.java
+++ app/src/main/java/teamseth/cs340/tickettoride/Activity/LoginActivity.java
@@ -3,7 +3,6 @@ package teamseth.cs340.tickettoride.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import teamseth.cs340.tickettoride.Fragment.LoginFragment;
 import teamseth.cs340.tickettoride.R;

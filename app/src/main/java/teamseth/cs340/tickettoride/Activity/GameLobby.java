c9e0d610b5eac30e4985e23c3c7748a6c84b8e83
andrew.olson <ajolson7@gmail.com>
2017-10-07 15:33:30 -0600

    Updated the Game Lobby files

--- app/src/main/java/teamseth/cs340/tickettoride/Activity/GameLobby.java
+++ app/src/main/java/teamseth/cs340/tickettoride/Activity/GameLobby.java
@@ -1,7 +1,33 @@
 package teamseth.cs340.tickettoride.Activity;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import teamseth.cs340.tickettoride.Fragment.GameLobbyFragment;
+import teamseth.cs340.tickettoride.R;
+
 /**
  * Created by Seth on 9/29/2017.
  */
-public class GameLobby {
+public class GameLobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_lobby);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.game_lobby_fragment_container);
+
+        if (fragment == null) {
+            fragment = new GameLobbyFragment();
+            fm.beginTransaction()
+                    .add(R.id.game_lobby_fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+
 }

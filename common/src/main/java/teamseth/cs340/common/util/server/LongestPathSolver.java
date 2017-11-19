5a1b1ad43a05a979e14cd198b4edd7dd1c7e25bc
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-11-18 23:55:38 -0700

    fixed longest path solver and rebased

--- common/src/main/java/teamseth/cs340/common/util/server/LongestPathSolver.java
+++ common/src/main/java/teamseth/cs340/common/util/server/LongestPathSolver.java
@@ -14,22 +14,22 @@ public class LongestPathSolver {
     public static final int calculateLongestPath(Set<Route> routeSet){
         Stream<Route> visited = Stream.empty();
         Stream<Route> routes = routeSet.stream();
-        Stream<Integer> solutions = routes.flatMap((Route route) -> recurseOnRoute(routes, route, visited)).map((Tuple<Integer, Set<Route>> solution) -> solution.get1());
+        Stream<Integer> solutions = routes.flatMap((Route route) -> recurseOnRoute(routes, route, visited)).map((Tuple<Integer, Stream<Route>> solution) -> solution.get1());
         Integer max = solutions.max((Integer solution1, Integer solution2) -> Integer.compare(solution1, solution2)).orElseGet(() -> -1);
         return max;
     }
 
-    private static final Stream<Tuple<Integer, Set<Route>>> recurseOnRoute(Stream<Route> routes, Route route, Stream<Route> visited) {
+    private static final Stream<Tuple<Integer, Stream<Route>>> recurseOnRoute(Stream<Route> routes, Route route, Stream<Route> visited) {
         Stream<Route> neighborRoutes = routes.filter((Route currentRoute) -> (currentRoute.hasCity(route.getCity1()) || currentRoute.hasCity(route.getCity2())));
         Stream<Route> currentVisited = Stream.concat(visited, Stream.of(route));
-        Stream<Tuple<Integer, Set<Route>>> output = neighborRoutes.filter((Route currentRoute) -> {
+        Stream<Tuple<Integer, Stream<Route>>> output = neighborRoutes.filter((Route currentRoute) -> {
             boolean hasBeenVisited = currentVisited.noneMatch((visitedRoute) -> visitedRoute.compareCitiesAndColor(currentRoute));
             return hasBeenVisited;
         }).flatMap((Route currentRoute) -> {
-            Stream<Tuple<Integer, Set<Route>>> result = recurseOnRoute(routes, currentRoute, currentVisited);
-            return result.map((Tuple<Integer, Set<Route>> singleResult) -> new Tuple<>(singleResult.get1() + route.getLength(), singleResult.get2()));
+            Stream<Tuple<Integer, Stream<Route>>> result = recurseOnRoute(routes, currentRoute, currentVisited);
+            return result.map((Tuple<Integer, Stream<Route>> singleResult) -> new Tuple<>(singleResult.get1() + route.getLength(), singleResult.get2()));
         });
-        return Stream.concat(output, Stream.of(new Tuple(route.getLength(), visited)));
+        return Stream.concat(output, Stream.of(new Tuple<>(route.getLength(), visited)));
     }
 
     // You can probably get away without returning the set of visited routes but in case we need it:

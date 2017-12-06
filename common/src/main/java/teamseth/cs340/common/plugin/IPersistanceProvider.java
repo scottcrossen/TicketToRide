46c0d8cdf3f7d10f944e1e30bae5a9c1e0cb8832
Scott Leland Crossen <scottcrossen42@gmail.com>
2017-12-06 12:27:02 -0700

    added persistance provider interface

---
+++ common/src/main/java/teamseth/cs340/common/plugin/IPersistanceProvider.java
@@ -0,0 +1,9 @@
+package teamseth.cs340.common.plugin;
+
+/**
+ * @author Scott Leland Crossen
+ * @copyright (C) Copyright 2017 Scott Leland Crossen
+ */
+public interface IPersistanceProvider {
+    void initialize();
+}

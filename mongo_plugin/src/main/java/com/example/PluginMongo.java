00800ecf729cb3f10e81cfbd635ea6ad6700d9e5
Mike Crowther <macrowther7+git@gmail.com>
2017-12-06 12:50:08 -0700

    basic MongoDB connection

--- mongo_plugin/src/main/java/com/example/PluginMongo.java
+++ mongo_plugin/src/main/java/com/example/PluginMongo.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public class PluginMongo implements IPersistanceProvider{
+    MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+    DB db = mongo.getDB("TicketToRide");
 
     @Override
     public void initialize() {
